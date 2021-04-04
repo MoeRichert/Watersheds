403038bb0f91545075d862a376080d1a2798082e
Moe Richert <46945488+MoeRichert@users.noreply.github.com>
2021-04-04 17:24:31 -0500

    improve processing speed

--- main.js
+++ main.js
@@ -402,34 +402,6 @@ function fineDetails(){
                 
         console.log(selectedNode.properties.tohuc);
         
-        
-        // TEST CODE Iterative function to implement Binary Search
-           /* function getHucIndex(target, x) {
-            arr=target.getLayers();
-
-                let start=0, end=arr.length-1;
-
-                // Iterate while start not meets end
-                while (start<=end){
-
-                    // Find the mid index
-                    let mid=Math.floor((start + end)/2);
-
-                    // If element is present at mid, return True
-                    if (arr[mid].feature.properties.huc===x) return mid;
-
-                    // Else look in left or right half accordingly
-                    else if (arr[mid].huc < x) 
-                         start = mid + 1;
-                    else
-                         end = mid - 1;
-                }
-
-                return -1;
-            }*/
-        
-        //END TEST CODE
-        
 
         function getHucIndex(arr, toHuc){
             for(let i = 0; i < arr.length; i++){
